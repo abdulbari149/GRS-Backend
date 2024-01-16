fd0a15bbc1427b630e4466b9442cfff34db5217e
abdulbari149 <abdulbari131103@gmail.com>
2024-01-16 15:18:17 +0500

    fix: source not found

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Running production build"
 # use pm2 to start python flask app
-source env/bin/activate
+chmod +x env/bin/activate
+./env/bin/activate
 pip install -r requirements.txt
 # run app.py with pm2
 pm2 start app.py --interpreter=env/bin/python3 --name="grs" 
